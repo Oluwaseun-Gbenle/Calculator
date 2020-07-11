292473dd8f2a588dd750c731b66e56150ba597e4
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-07-11 13:21:24 +0100

    Updates

---
+++ precache-manifest.f33df0d98ad8273b4f2748b0b229fea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bff45987fb7a693b5b645645ae744dc",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "17ca523328f9fe60e747",
+    "url": "/Calculator/static/css/main.7523452a.chunk.css"
+  },
+  {
+    "revision": "ed34ad91e9020be8ea88",
+    "url": "/Calculator/static/js/2.c3788b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.c3788b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ca523328f9fe60e747",
+    "url": "/Calculator/static/js/main.6214fcc6.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
